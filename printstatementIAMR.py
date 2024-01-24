0c56228c8ec565bb5ec6ae0fb9ccbdff845a123d
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-01-24 17:07:05 +0530

    Add files via upload

---
+++ printstatementIAMR.py
@@ -0,0 +1,45 @@
+a = 65
+i,k,j,p = 3,6,5,9
+#(multiple variable assignment)
+print('This is python language..',a)
+print(i,k,j,p)
+#this is print method(Single line comment)
+'''ksjdfhier
+fghgthtyh
+fdfgbbgjh
+fgbgghnjjhjhyfdf
+fgrtht'''
+#multi-line comment
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+""")
+print("sum of and b is:",a,end = " ")
+print("jshdfbiudg")
+a = 6
+b = 4
+c = a+b
+d = a/b
+print(c)
+print(d)
+print("Sum of a and b is:",c)
+print("Sum of",a ,"and",b,"is:",c)
+print("Sum of %d and %d is %d"%(a,b,c))
+print("Div of %d and %d is %f"%(a,b,d))
+print("Div of {} and {} is {}".format(a,d,b))
+#f-string
+print(f"Div of {a} and {b} is {d}")
+#walrus operator
+a = 5
+b = 3
+
+print(z := a+b)
+print(f"sum of {a} and {b} is {(z := a + b)}")
+
+#input
+Val = int(input("Enter the value.."))
+Val_1 = int(input("Enter the value 1.."))
+print(Val + Val_1)
+
+
