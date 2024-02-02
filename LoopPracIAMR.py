ab5be758fe36513129d7bb4406df72eabc51982f
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-02-02 17:06:59 +0530

    Add files via upload

---
+++ LoopPracIAMR.py
@@ -0,0 +1,19 @@
+import math
+n = int(input("enter the range.."))
+'''sums = 0
+for i in range(2,n,2):
+    sums += i
+print(sums)'''
+
+'''for i in range(1,n+1):
+    print(i**3)'''
+'''for i in range(1,n):
+    print(int(math.pow(i,3)))'''
+rem = 0
+rev = 0
+while n > 0:
+    rem = n%10
+    rev = rev*10 + rem
+    n //= 10
+    
+print(rev)
